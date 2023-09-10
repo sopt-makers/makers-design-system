306f4914a076912aec8a8250e71d0e4a4a5d902a
Jun <jun.juno@kakaostyle.com>
2023-09-11 02:02:35 +0900

    feat(colors): version 0.0.0 npm publish 추가 (#2)
    * feat: colors 예시 추가
    
    * chore: changeset 설치
    
    * feat: colors 1.0.0
    
    * feat: makers colors npm publish 결과 추가

---
+++ packages/colors/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @sopt-makers/colors
+
+## 0.0.0
+
+### Major Changes
+
+- release first makers colors
---
+++ packages/colors/src/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  black: "#000000",
+  white: "#ffffff",
+  blue50: "#346DFF",
+  primary: "#FF6E1D",
+};
