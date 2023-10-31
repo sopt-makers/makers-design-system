7aa2d429d5d708771a32538553935faf4693bb4d
sohee <soheek@fairsquarelab.com>
2023-10-31 10:42:42 +0900

    feat(fonts): release 1.0.0

---
+++ .changeset/bright-papayas-cough.md
@@ -0,0 +1,5 @@
+---
+"@sopt-makers/fonts": major
+---
+
+release v.1.0.0
---
+++ packages/fonts/src/fonts.ts
@@ -0,0 +1,175 @@
+export const fontBase = `
+  font-family: 'SUIT', sans-serif;
+  font-style: normal;
+`;
+
+export const fonts = {
+  HEADING_48_B: `
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 72px;
+    letter-spacing: -2%;
+  `,
+  HEADING_32_B: `
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 48px;
+    letter-spacing: -2%;
+  `,
+  HEADING_28_B: `
+    font-weight: 700;
+    font-size: 28px;
+    line-height: 42px;
+    letter-spacing: -2%;
+  `,
+  HEADING_24_B: `
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 36px;
+    letter-spacing: -2%;
+  `,
+  HEADING_20_B: `
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 30px;
+    letter-spacing: -2%;
+  `,
+  HEADING_18_B: `
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 28px;
+    letter-spacing: -2%;
+  `,
+  HEADING_16_B: `
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: -2%;
+  `,
+  TITLE_32_SB: `
+    font-weight: 600;
+    font-size: 32px;
+    line-height: 48px;
+    letter-spacing: -2%;
+  `,
+  TITLE_28_SB: `
+    font-weight: 600;
+    font-size: 28px;
+    line-height: 42px;
+    letter-spacing: -2%;
+  `,
+  TITLE_24_SB: `
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 36px;
+    letter-spacing: -2%;
+  `,
+  TITLE_20_SB: `
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 30px;
+    letter-spacing: -2%;
+  `,
+  TITLE_18_SB: `
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 28px;
+    letter-spacing: -2%;
+  `,
+  TITLE_16_SB: `
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: -1.5%;
+  `,
+  TITLE_14_SB: `
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: -1.5%;
+  `,
+  BODY_18_M: `
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 30px;
+    letter-spacing: -1.5%;
+  `,
+  BODY_16_M: `
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 26px;
+    letter-spacing: -1.5%;
+  `,
+  BODY_16_R: `
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 26px;
+    letter-spacing: -1.5%;
+  `,
+  BODY_14_M: `
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 22px;
+    letter-spacing: -1.5%;
+  `,
+  BODY_14_R: `
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 22px;
+    letter-spacing: -1.5%;
+  `,
+  BODY_14_L: `
+    font-weight: 300;
+    font-size: 14px;
+    line-height: 22px;
+    letter-spacing: -1.5%;
+  `,
+  BODY_13_M: `
+    font-weight: 500;
+    font-size: 13px;
+    line-height: 20px;
+    letter-spacing: -1.5%;
+  `,
+  BODY_13_R: `
+    font-weight: 400;
+    font-size: 13px;
+    line-height: 20px;
+    letter-spacing: -1.5%;
+  `,
+  BODY_13_L: `
+    font-weight: 300;
+    font-size: 13px;
+    line-height: 20px;
+    letter-spacing: -1.5%;
+  `,
+  LABEL_18_SB: `
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 24px;
+    letter-spacing: -2%;
+  `,
+  LABEL_16_SB: `
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 22px;
+    letter-spacing: -2%;
+  `,
+  LABEL_14_SB: `
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 18px;
+    letter-spacing: -2%;
+  `,
+  LABEL_12_SB: `
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 16px;
+    letter-spacing: -2%;
+  `,
+  LABEL_11_SB: `
+    font-weight: 600;
+    font-size: 11px;
+    line-height: 14px;
+    letter-spacing: -2%;
+  `,
+}
