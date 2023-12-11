61a141713135fbfa618849bfec4c6ccbb5fca801
solar3070 <>
2023-12-12 01:48:02 +0900

    fix: 컴포넌트 상대 경로 수정

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,8 +1,8 @@
 export * from "./cssVariables";
 
 // component exports
-// export { default as Button } from "../Button";
-// export { ToastProvider, useToast } from "../Toast";
-// export type { ToastOptionType } from "../Toast";
-// export { default as Test } from "./Test";
-// export { default as Dialog } from "../Dialog";
+export { default as Button } from "./Button";
+export { ToastProvider, useToast } from "./Toast";
+export type { ToastOptionType } from "./Toast";
+export { default as Test } from "./Test";
+export { default as Dialog } from "./Dialog";
