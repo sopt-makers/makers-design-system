f2e2cbd3c7c5167677d3e9c2ed9cfdfeb904a8a9
solar3070 <>
2023-12-12 01:11:56 +0900

    feat: 데스크탑 및 모바일 뷰 CSS Variable 추가

---
+++ packages/ui/cssVariables.ts
@@ -0,0 +1,9 @@
+export const desktopVariables = `:root {
+  --mds-toast-width: 380px;
+  --mds-toast-top: 36px;
+}`;
+
+export const mobileVariables = `:root {
+  --mds-toast-width: 343px;
+  --mds-toast-top: 16px;
+}`;
