761ff54862167ef78571e641239824cb3cba5ff4
solar3070 <>
2024-02-25 23:49:07 +0900

    chore: 테스트 코드 수정

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,16 @@
-import { Test } from 'ui';
-import styles from './page.module.css';
+"use client";
+
+import { ToastProvider } from "ui";
+import styles from "./page.module.css";
+import ToastTemp from "./ToastTemp";
 
 export default function Page(): JSX.Element {
+
   return (
-    <main className={styles.main}>
-      <Test color="blue" size="big" text="Test Component" />
-    </main>
+    <ToastProvider>
+      <main className={styles.main}>
+      <ToastTemp />
+      </main>
+    </ToastProvider>
   );
 }
