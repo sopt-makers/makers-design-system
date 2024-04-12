f8cc96667cf05079fee592f03b08fe02145655c9
sohee <soheek@fairsquarelab.com>
2024-04-12 11:38:40 +0900

    [DOCS] chore: update package.json

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -1,10 +1,12 @@
 import { Test } from '@sopt-makers/ui';
 import './App.css';
+import TextField from '../../../packages/ui/Input/TextField';
 
 function App() {
   return (
     <>
       <Test text="Test Component" size="big" color="blue" />
+      <TextField labelText="Label" errorMessage="error message" />
     </>
   );
 }
