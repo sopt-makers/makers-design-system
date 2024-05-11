4e225ea35940a63b7455945b6455d37999dd7c41
Brokyeom <goindol40@gmail.com>
2024-05-11 14:52:41 +0900

    chore : migrate ui package bundler
    - change bundler from rollup
    - use tsup

---
+++ packages/ui/tsup.config.ts
@@ -0,0 +1,18 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/esbuild-plugin';
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['index.ts', "cssVariables.ts"],
+  format: ['cjs', 'esm'], // CommonJS와 ES Module 형식으로 출력
+  dts: true, 
+  sourcemap: true, 
+  esbuildPlugins: [vanillaExtractPlugin()],
+  clean: true, 
+  external: ['react', 'react-dom', '@radix-ui/*'], // nodeExternals와 유사하게 외부 모듈을 제외
+  onSuccess: 'echo Build completed!', 
+
+  esbuildOptions(options) {
+    options.minify = true;
+    options.treeShaking = true;
+  },
+});
