63e1e7ebf01639706226ecb545ebea0c8d1d654e
Jeon Eonseok <jseokeon@gmail.com>
2024-05-31 00:24:41 +0900

    feat: Chip component 구현

--- packages/ui/Chip/Chip.tsx
+++ packages/ui/Chip/Chip.tsx
@@ -1,13 +1,42 @@
-import * as styles from './style.css';
+import type { ButtonHTMLAttributes, ReactNode } from 'react';
+import { IconCheck } from '@sopt-makers/icons';
+import { checkedStyle, root, sprinkles } from './style.css';
 
-interface ChipProps {
-  children?: React.ReactNode;
+interface ChipProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: ReactNode;
+  className?: string;
+  size?: 'sm' | 'md';
+  iconLocation?: 'none' | 'left' | 'right';
+  isChecked?: boolean;
 }
 
-function Chip(props: ChipProps) {
-  const { children } = props;
-
-  return <div className={styles.root}>{children}</div>;
+function Chip({
+  children,
+  className,
+  size = 'md',
+  iconLocation = 'none',
+  isChecked = false,
+  ...buttonElementProps
+}: ChipProps) {
+  return (
+    <button
+      className={`${root} ${className} ${sprinkles({
+        padding: size,
+        fontSize: size,
+        lineHeight: size,
+      })} ${isChecked && checkedStyle}`}
+      type='button'
+      {...buttonElementProps}
+    >
+      {isChecked && iconLocation === 'left' ? (
+        <IconCheck style={{ width: '16px', height: '16px' }} />
+      ) : null}
+      <span>{children}</span>
+      {isChecked && iconLocation === 'right' ? (
+        <IconCheck style={{ width: '16px', height: '16px' }} />
+      ) : null}
+    </button>
+  );
 }
 
 export default Chip;
