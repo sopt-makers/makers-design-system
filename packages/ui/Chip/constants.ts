2b79971ed45f283e8dfda5ee0213087ba3ba6693
Jeon Eonseok <jseokeon@gmail.com>
2024-05-31 00:23:59 +0900

    feat: sprinkle 속성에 대한 css 지정

---
+++ packages/ui/Chip/constants.ts
@@ -0,0 +1,16 @@
+import type { ChipSizeTheme } from './types';
+
+export const paddings: Record<ChipSizeTheme, string> = {
+  sm: '9px 14px',
+  md: '10px 20px',
+};
+
+export const fontSizes: Record<ChipSizeTheme, string> = {
+  sm: '14px',
+  md: '16px',
+};
+
+export const lineHeights: Record<ChipSizeTheme, string> = {
+  sm: '18px',
+  md: '22px',
+};
