77bc170cc9b5279853f55d9d29790810dd03e56d
lydiacho <sheecho0804@ewhain.net>
2024-06-10 17:09:32 +0900

    docs: Callout storybook 작성

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -7,7 +7,8 @@ export { Dialog, DialogContext, DialogProvider, useDialog } from "./Dialog";
 export type { DialogOptionType } from "./Dialog";
 export { ToastProvider, useToast, Toast } from "./Toast";
 export type { ToastOptionType } from "./Toast";
-export { TextField, TextArea, SearchField } from './Input';
+export { TextField, TextArea, SearchField } from "./Input";
+export { default as Callout } from "./Callout";
 
 // test component
 export { default as Test } from "./Test";
