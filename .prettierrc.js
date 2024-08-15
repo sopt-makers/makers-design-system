11efa7e1e1b429bb282e56a157ec7dd04cb0012c
Brokyeom <goindol40@gmail.com>
2024-08-15 12:51:59 +0900

    add prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Options} */
+const config = {
+  arrowParens: "always",
+  endOfLine: "lf",
+  jsxSingleQuote: true,
+  printWidth: 120,
+  quoteProps: "consistent",
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+};
+
+module.exports = config;
