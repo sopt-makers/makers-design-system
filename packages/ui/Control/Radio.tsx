cb847277df67517fd67bf582a748ad32f691e06b
HyeongKyeom Kim <97586683+Brokyeom@users.noreply.github.com>
2024-08-20 23:05:37 +0900

    fix(Radio): label 입력이 정상적으로 되지 않던 문제 수정 (#116)
    * fix: Radio 컴포넌트 label이 정상적으로 작동하지 않던 문제 수정
    
    * changeset

--- packages/ui/Control/Radio.tsx
+++ packages/ui/Control/Radio.tsx
@@ -1,34 +1,29 @@
-import type { InputHTMLAttributes } from "react";
-import { forwardRef } from "react";
-import theme from "../theme.css";
-import { controlLabel, controlWrapper, radio } from "./style.css";
+import type { InputHTMLAttributes } from 'react';
+import { forwardRef } from 'react';
+import theme from '../theme.css';
+import { controlLabel, controlWrapper, radio } from './style.css';
 
-export interface RadioProps
-  extends Omit<InputHTMLAttributes<HTMLInputElement>, "size"> {
+export interface RadioProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
   label?: string;
-  size?: "sm" | "lg";
+  size?: 'sm' | 'lg';
   checked?: boolean;
   color?: string;
 }
 
 const Radio = forwardRef<HTMLInputElement, RadioProps>(
-  ({ checked = false, label, size = "sm", color = theme.colors.gray10, ...props }, ref) => {
+  ({ checked = false, label, size = 'sm', color = theme.colors.gray10, ...props }, ref) => {
     return (
       <label className={controlWrapper}>
-        <input
-          checked={checked}
-          className={radio[size]}
-          ref={ref}
-          type="radio"
-          {...props}
-        />
+        <input checked={checked} className={radio[size]} ref={ref} type='radio' {...props} />
         {label ? (
-          <p className={controlLabel[size]} style={{ color }}>label</p>
+          <p className={controlLabel[size]} style={{ color }}>
+            {label}
+          </p>
         ) : null}
       </label>
     );
-  }
+  },
 );
-Radio.displayName = "Radio";
+Radio.displayName = 'Radio';
 
 export default Radio;
