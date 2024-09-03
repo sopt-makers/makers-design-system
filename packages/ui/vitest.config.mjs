23e8587f9d9abbdd85de00d78e78af8db9db749c
jungwoo3490 <jungwoo3490@naver.com>
2024-09-04 02:33:50 +0900

    chore(ui): config 파일 추가

---
+++ packages/ui/vitest.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['**/node_modules/**', '**/dist/**'],
+  },
+});
