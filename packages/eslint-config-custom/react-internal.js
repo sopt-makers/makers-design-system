cef6c616c54ec7f9599ff1898ba51628904275f3
jungwoo3490 <jungwoo3490@naver.com>
2024-09-04 05:03:36 +0900

    chore(eslint-config-custom): Vitest eslint rule 추가

--- packages/eslint-config-custom/react-internal.js
+++ packages/eslint-config-custom/react-internal.js
@@ -1,6 +1,6 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path');
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json');
 
 /*
  * This is a custom ESLint configuration for use with
@@ -14,9 +14,10 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 module.exports = {
   extends: [
-    "@vercel/style-guide/eslint/browser",
-    "@vercel/style-guide/eslint/typescript",
-    "@vercel/style-guide/eslint/react",
+    '@vercel/style-guide/eslint/browser',
+    '@vercel/style-guide/eslint/typescript',
+    '@vercel/style-guide/eslint/react',
+    '@vercel/style-guide/eslint/vitest',
   ].map(require.resolve),
   parserOptions: {
     project,
@@ -25,27 +26,27 @@ module.exports = {
     JSX: true,
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         project,
       },
     },
   },
-  ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js"],
+  ignorePatterns: ['node_modules/', 'dist/', '.eslintrc.js'],
 
   rules: {
-    "import/no-default-export": "off",
-    "import/prefer-default-export": "warn",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    "unicorn/filename-case": [
-      "error",
+    'import/no-default-export': 'off',
+    'import/prefer-default-export': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'unicorn/filename-case': [
+      'error',
       {
-        "cases": {
-          "camelCase": true,
-          "pascalCase": true
-        }
-      }
-    ]
+        cases: {
+          camelCase: true,
+          pascalCase: true,
+        },
+      },
+    ],
   },
 };
