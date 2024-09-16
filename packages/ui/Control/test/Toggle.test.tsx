7bc231be5ed21cbc15a7b8673c22d5357cb6b786
Brokyeom <goindol40@gmail.com>
2024-09-16 20:08:48 +0900

    add Toggle test code.

---
+++ packages/ui/Control/test/Toggle.test.tsx
@@ -0,0 +1,24 @@
+import { describe } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import type { ToggleProps } from 'Control/Toggle';
+import { Toggle } from '../index';
+
+describe('Toggle의', () => {
+  function renderToggle(props?: ToggleProps) {
+    render(<Toggle {...props} />);
+  }
+
+  describe('기본 동작은 다음과 같다.', () => {
+    it('on 상태로 렌더링 될 수 있다.', () => {
+      renderToggle({ 'aria-checked': 'true' });
+
+      expect(screen.getByRole('switch')).toHaveAttribute('aria-checked', 'true');
+    });
+
+    it('off 상태로 렌더링 될 수 있다.', () => {
+      renderToggle({ 'aria-checked': 'false' });
+
+      expect(screen.getByRole('switch')).toHaveAttribute('aria-checked', 'false');
+    });
+  });
+});
