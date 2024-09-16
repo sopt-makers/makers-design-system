b523b926c44223707061a3055cf8588ef192d93e
Brokyeom <goindol40@gmail.com>
2024-09-16 16:24:00 +0900

    fix: checked, aria-checked 속성 추가

--- packages/ui/Control/CheckBox.tsx
+++ packages/ui/Control/CheckBox.tsx
@@ -1,39 +1,40 @@
-import type { InputHTMLAttributes } from "react";
-import { forwardRef } from "react";
-import { IconCheck } from "@sopt-makers/icons";
-import theme from "../theme.css";
-import {
-  check,
-  checkBox,
-  checkBoxChecked,
-  checkBoxInput,
-  controlLabel,
-  controlWrapper,
-} from "./style.css";
+import type { InputHTMLAttributes } from 'react';
+import { forwardRef } from 'react';
+import { IconCheck } from '@sopt-makers/icons';
+import theme from '../theme.css';
+import { check, checkBox, checkBoxChecked, checkBoxInput, controlLabel, controlWrapper } from './style.css';
 
-export interface CheckBoxProps
-  extends Omit<InputHTMLAttributes<HTMLInputElement>, "size"> {
+export interface CheckBoxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
   label?: string;
-  size?: "sm" | "lg";
+  size?: 'sm' | 'lg';
   checked?: boolean;
   color?: string;
 }
 
 const CheckBox = forwardRef<HTMLInputElement, CheckBoxProps>(
-  ({ checked = false, label, size = "sm", color = theme.colors.gray10, ...props }, ref) => {
+  ({ checked = false, label, size = 'sm', color = theme.colors.gray10, ...props }, ref) => {
     return (
       <label className={controlWrapper}>
-        <input className={checkBoxInput} ref={ref} type="checkbox" {...props} />
+        <input
+          aria-checked={checked}
+          checked={checked}
+          className={checkBoxInput}
+          ref={ref}
+          type='checkbox'
+          {...props}
+        />
         <div className={`${checkBox[size]} ${checkBoxChecked[`${checked}`]}`}>
           {checked ? <IconCheck className={check[size]} /> : null}
         </div>
         {label ? (
-          <p className={controlLabel[size]} style={{ color }}>{label}</p>
+          <p className={controlLabel[size]} style={{ color }}>
+            {label}
+          </p>
         ) : null}
       </label>
     );
-  }
+  },
 );
-CheckBox.displayName = "CheckBox";
+CheckBox.displayName = 'CheckBox';
 
 export default CheckBox;
