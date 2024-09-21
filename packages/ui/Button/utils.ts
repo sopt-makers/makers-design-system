0dec2d412a6774ae17861fc8cf8a668a2abb9df3
jungwoo3490 <jungwoo3490@naver.com>
2024-09-21 10:36:23 +0900

    chore(Button): 주석 제거

--- packages/ui/Button/utils.ts
+++ packages/ui/Button/utils.ts
@@ -7,10 +7,6 @@ function createButtonVariant(
   sizeTheme: ButtonSizeTheme,
   styleVariant: 'fill' | 'outlined',
 ) {
-  // 추후 추가되는 디자인 테마 타입에 따라 prefix를 간편하게 추가해주면 되도록 구현
-  // (단, constants.ts에서 해당 prefix가 붙은 스타일 상수를 사전 정의해야 함)
-  // const typedColor: ButtonTypedColorTheme = outlined ? `outlined-${colorTheme}` : colorTheme;
-
   return sprinkles({
     backgroundColor: {
       default: `${styleVariant}-${colorTheme}-default`,
