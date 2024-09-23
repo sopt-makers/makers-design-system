80bfddcdd88a37df6f1b27da02fba41de20a35b6
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-09-23 23:50:00 +0900

    feat(Select): deprecated 폴더 추가 및 기존 컴포넌트 생성, jsdocs로 설명추가

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -2,16 +2,16 @@ export * from './cssVariables';
 
 // component exports
 export { default as Button } from './Button';
-export { CheckBox, Toggle, Radio } from "./Control";
+export { CheckBox, Toggle, Radio } from './Control';
 export { Dialog, DialogContext, DialogProvider, useDialog } from './Dialog';
 export type { DialogOptionType } from './Dialog';
 export { ToastProvider, useToast, Toast } from './Toast';
 export type { ToastOptionType } from './Toast';
-export { TextField, TextArea, SearchField, Select, UserMention } from "./Input";
-export { default as Tag } from "./Tag";
+export { TextField, TextArea, SearchField, SelectV2, Select, UserMention } from './Input';
+export { default as Tag } from './Tag';
 export { default as Chip } from './Chip';
-export { default as Callout } from "./Callout";
-export { default as Tab } from "./Tab";
+export { default as Callout } from './Callout';
+export { default as Tab } from './Tab';
 
 // test component
 export { default as Test } from './Test';
