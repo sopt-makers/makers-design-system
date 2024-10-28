c53bfe3e247630fc277f53d44b290316cdefcbe4
HyeongKyeom Kim <97586683+Brokyeom@users.noreply.github.com>
2024-10-28 23:22:47 +0900

    Revert "feat(ui): Skeleton 컴포넌트 구현 (#178)" (#189)
    This reverts commit be92ccfa6358fe4abd98173846dd13a9e3545a8b.

--- packages/ui/Skeleton/constants.ts
+++
@@ -1,9 +0,0 @@
-import type { SkeletonVariant } from './types';
-
-const SkeletonBorderRadius: Record<SkeletonVariant, string> = {
-  default: '12px',
-  circular: '100%',
-  rounded: '100%',
-};
-
-export default SkeletonBorderRadius;
--- packages/ui/Skeleton/utils.ts
+++
@@ -1,8 +0,0 @@
-import { sprinkles } from './style.css';
-import type { SkeletonVariant } from './types';
-
-const createSkeletonStyle = (variant: SkeletonVariant) => {
-  return sprinkles({ borderRadius: variant });
-};
-
-export default createSkeletonStyle;
