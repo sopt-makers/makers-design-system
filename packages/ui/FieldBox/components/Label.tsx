a370331b56c7e6cb0f0209937a47595a58444b65
Brokyeom <goindol40@gmail.com>
2024-10-29 16:51:28 +0900

    fix: FieldBox.Label Optional prop으로 수정

--- packages/ui/FieldBox/components/Label.tsx
+++ packages/ui/FieldBox/components/Label.tsx
@@ -3,13 +3,13 @@ import { forwardRef } from 'react';
 import { requiredMarkStyle, TopAddonDescriptionStyle, TopAddonLabelStyle } from '../style.css';
 
 export interface FieldBoxLabelProps extends HTMLAttributes<HTMLDivElement> {
-  label: string;
-  description: string;
-  required: boolean;
+  label?: string;
+  description?: string;
+  required?: boolean;
 }
 
 export const FieldBoxLabel = forwardRef<HTMLDivElement, FieldBoxLabelProps>((props, forwardedRef) => {
-  const { required, label, description } = props;
+  const { required = false, label, description } = props;
 
   return (
     <div aria-label={label} aria-required={required} ref={forwardedRef}>
