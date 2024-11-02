c820559381296a66ee957ef09d6a6358ddafd062
Brokyeom <goindol40@gmail.com>
2024-11-03 00:21:27 +0900

    fix: Extend HTMLDivElement in Tag Component

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -8,11 +8,11 @@ export type { DialogOptionType } from './Dialog';
 export { ToastProvider, useToast, Toast } from './Toast';
 export type { ToastOptionType } from './Toast';
 export { TextField, TextArea, SearchField, SelectV2, Select, UserMention } from './Input';
-export { default as Tag } from './Tag';
 export { default as Chip } from './Chip';
 export { default as Callout } from './Callout';
 export { default as Tab } from './Tab';
 export * from './Skeleton';
 export * from './FieldBox';
+export * from './Tag';
 // test component
 export { default as Test } from './Test';
