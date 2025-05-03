6ce0411ad43b80d9d6dc83f1401745d891f8bd1f
minjeoong <orange_mj@naver.com>
2025-05-04 04:53:01 +0900

    feat: 스토리북 작성

--- apps/docs/src/stories/SelectV2WithTriggerContent.stories.tsx
+++ apps/docs/src/stories/SelectV2WithTriggerContent.stories.tsx
@@ -80,6 +80,39 @@ const userOptions = [
   },
 ];
 
+const multipleOptions = [
+  {
+    label: 'Option 1',
+    value: 'option1',
+    description: 'Description 1',
+  },
+  {
+    label: 'Option 2',
+    value: 'option2',
+    description: 'Description 2',
+  },
+  {
+    label: 'Option 3',
+    value: 'option3',
+    description: 'Description 3',
+  },
+  {
+    label: 'Option 4',
+    value: 'option4',
+    description: 'Description 4',
+  },
+  {
+    label: 'Option 5',
+    value: 'option5',
+    description: 'Description 5',
+  },
+  {
+    label: 'Option 6',
+    value: 'option6',
+    description: 'Description 6',
+  },
+];
+
 const meta: Meta<typeof SelectV2.Root> = {
   title: 'Components/Input/SelectV2/WithTriggerContent',
   component: SelectV2.Root,
@@ -87,10 +120,25 @@ const meta: Meta<typeof SelectV2.Root> = {
   args: {
     visibleOptions: 3,
     onChange: fn(),
+    multiple: false,
   },
   argTypes: {
     defaultValue: { control: 'select', options: ['', 'option1', 'option2', 'option3', 'option4', 'option5'] },
-    visibleOptions: { control: 'number' },
+    visibleOptions: { control: 'number', description: '보여줄 옵션 개수' },
+    onChange: {
+      control: false,
+      description: 'onChange 이벤트 핸들러',
+    },
+    multiple: { control: 'boolean', description: '다중선택으로 쓸 때 true 로 설정' },
+    placeholder: {
+      control: 'text',
+      description: 'placeholder',
+    },
+    type: {
+      control: 'radio',
+      options: ['text', 'textDesc', 'textIcon', 'userList', 'userListDesc'],
+      description: 'select 컴포넌트 type',
+    },
   },
 };
 
@@ -208,5 +256,29 @@ export const UserListDesc: StoryObj<SelectStoryArgs> = {
   },
   argTypes: {
     type: { control: 'radio', options: ['userList', 'userListDesc'] },
+  },
+};
+
+export const Multiple: StoryObj<SelectStoryArgs> = {
+  render: ({ placeholder, ...args }) => (
+    <SelectV2.Root {...args} multiple={true}>
+      <SelectV2.Trigger>
+        <SelectV2.TriggerContent placeholder={placeholder} label={'라벨'} />
+      </SelectV2.Trigger>
+      <SelectV2.Menu>
+        {multipleOptions.map((option) => (
+          <SelectV2.MenuItem key={option.value} option={option} />
+        ))}
+      </SelectV2.Menu>
+    </SelectV2.Root>
+  ),
+  args: {
+    type: 'text',
+    visibleOptions: 3,
+    placeholder: 'placeholder',
+    multiple: true,
+  },
+  argTypes: {
+    multiple: { control: true },
   },
 };
