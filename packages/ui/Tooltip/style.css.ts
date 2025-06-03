4b1b2e88a88faae7612a2cded9abbd63dc669d9e
namdaeun <nde40345@gmail.com>
2025-06-04 00:08:04 +0900

    style: 툴팁 등장 애니메이션 추가

--- packages/ui/Tooltip/style.css.ts
+++ packages/ui/Tooltip/style.css.ts
@@ -1,4 +1,4 @@
-import { style } from '@vanilla-extract/css';
+import { style, styleVariants } from '@vanilla-extract/css';
 import theme from 'theme.css';
 
 export const tooltipWrapper = style({
@@ -12,6 +12,7 @@ export const trigger = style({
   display: 'flex',
   alignItems: 'center',
   gap: '4px',
+  whiteSpace: 'nowrap',
   color: theme.colors.gray300,
   ...theme.fontsObject.LABEL_4_12_SB,
 });
@@ -25,6 +26,33 @@ export const contentWrapper = style({
   padding: '16px',
   borderRadius: '10px',
   backgroundColor: theme.colors.gray600,
+  opacity: 0,
+  transform: 'translateY(-5px)',
+  transition: 'opacity 0.3s ease-in-out, transform 0.3s ease-in-out',
+  pointerEvents: 'auto',
+
+  selectors: {
+    '&[data-visible="true"]': {
+      opacity: 1,
+      transform: 'translateY(0)',
+      transition: 'opacity 0.3s ease-in-out, transform 0.3s ease-in-out',
+    },
+    '&[data-visible="true"]:hover': {
+      opacity: 1,
+      transform: 'translateY(0)',
+    },
+  },
+});
+
+export const contentWrapperVariant = styleVariants({
+  visible: {
+    visibility: 'visible',
+    pointerEvents: 'auto',
+  },
+  hidden: {
+    visibility: 'hidden',
+    pointerEvents: 'none',
+  },
 });
 
 export const bubblePointIcon = style({
