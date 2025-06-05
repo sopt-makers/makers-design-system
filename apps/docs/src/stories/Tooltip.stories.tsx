9166e1ff3e66b6f6023d907084a1469f777b91eb
namdaeun <nde40345@gmail.com>
2025-06-05 15:03:19 +0900

    feat: 툴팁 스토리북 파일 추가

---
+++ apps/docs/src/stories/Tooltip.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Tooltip } from '@sopt-makers/ui';
+
+const meta = {
+  title: 'Components/Tooltip',
+  component: Tooltip,
+  tags: ['autodocs'],
+  argTypes: {
+    triggerContent: {
+      control: 'text',
+      description: '툴팁을 트리거하는 텍스트입니다.',
+    },
+    children: {
+      control: 'text',
+      description: '툴팁 내용입니다.',
+    },
+  },
+} as Meta<typeof Tooltip>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    triggerContent: 'Tooltip',
+    children: 'Default Content',
+  },
+};
