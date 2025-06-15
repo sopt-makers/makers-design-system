3576b188082f1d1efda98a4c6bf8a0670a1ac552
namdaeun <nde40345@gmail.com>
2025-06-15 16:26:40 +0900

    chore: 에러메시지 한글로 변경

--- packages/ui/Tooltip/TooltipContext.tsx
+++ packages/ui/Tooltip/TooltipContext.tsx
@@ -11,7 +11,7 @@ export const TooltipContext = createContext<TooltipContextProps | null>(null);
 export const useTooltipContext = () => {
   const context = useContext(TooltipContext);
 
-  if (!context) throw new Error('Tooltip components must be used within Tooltip.Root');
+  if (!context) throw new Error('Tooltip 컴포넌트는 Tooltip.Root 내에서 사용되어야 합니다.');
 
   return context;
 };
