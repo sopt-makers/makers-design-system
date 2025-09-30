a783475d1f58bc73b26bc483d20ae41c88165367
최주용 <wuzoo@MacBook-Pro.local>
2025-09-30 18:16:33 +0900

    feat: context for bottom sheet open state control

---
+++ packages/ui/BottomSheet/context.tsx
@@ -0,0 +1,31 @@
+import { createContext, PropsWithChildren, useContext } from 'react';
+
+interface ContextProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const BottomSheetContext = createContext<ContextProps | null>(null);
+
+export function BottomSheetProvider({ open, onOpenChange, children }: PropsWithChildren<ContextProps>) {
+  return (
+    <BottomSheetContext.Provider
+      value={{
+        open,
+        onOpenChange,
+      }}
+    >
+      {children}
+    </BottomSheetContext.Provider>
+  );
+}
+
+export function useBottomSheetContext() {
+  const context = useContext(BottomSheetContext);
+
+  if (context === null) {
+    throw new Error('this hook muse be used within a BottomSheetProvider');
+  }
+
+  return context;
+}
