875c158b9533b1003827a3595fa52e5707337011
Yoo TaeSeung <remicon99@gmail.com>
2025-10-04 15:38:48 +0900

    feat: Tag 아이콘 gap 추가

--- packages/ui/Tag/style.css.ts
+++ packages/ui/Tag/style.css.ts
@@ -1,20 +1,14 @@
-import { createSprinkles, defineProperties } from "@vanilla-extract/sprinkles";
-import { style } from "@vanilla-extract/css";
-import {
-  bgColors,
-  borderRadiuses,
-  borders,
-  fonts,
-  paddings,
-  textColors,
-} from "./constants";
+import { createSprinkles, defineProperties } from '@vanilla-extract/sprinkles';
+import { style } from '@vanilla-extract/css';
+import { bgColors, borderRadiuses, borders, fonts, paddings, textColors } from './constants';
 
 export const root = style({
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  width: "fit-content",
-  height: "fit-content",
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: 'fit-content',
+  height: 'fit-content',
+  gap: '4px',
 });
 
 const sprinkleProperties = defineProperties({
