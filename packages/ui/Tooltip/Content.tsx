4a6c6274026c7e232fa2d371240fcbd6959fd902
namdaeun <nde40345@gmail.com>
2025-10-07 21:47:55 +0900

    feat: size prop 추가

--- packages/ui/Tooltip/Content.tsx
+++ packages/ui/Tooltip/Content.tsx
@@ -4,25 +4,30 @@ import BubblePointIcon from 'Tooltip/icons/bubblePoint';
 import clsx from 'clsx';
 import * as S from './style.css';
 import { useTooltipContext } from './TooltipContext';
-import { useTooltipContentPosition } from 'Tooltip/useTooltip';
+import { useTooltipContentPlacement } from 'Tooltip/useTooltipContentPlacement';
 
-const TooltipContent = forwardRef<HTMLDivElement, PropsWithChildren>(({ children }) => {
+export interface TooltipContentProps extends PropsWithChildren {
+  size?: 'small' | 'large';
+}
+
+const TooltipContent = forwardRef<HTMLDivElement, TooltipContentProps>(({ children, size = 'small' }) => {
   const { isOpen, tooltipId, contentRef } = useTooltipContext();
-  const { position } = useTooltipContentPosition();
+  const { placement } = useTooltipContentPlacement();
 
   return (
     <div
       aria-hidden={!isOpen}
       className={clsx(
         S.contentWrapper[isOpen ? 'visible' : 'hidden'],
-        S.contentWrapperPosition[position],
+        S.contentWrapperPosition[placement],
         S.commonContentWrapper,
+        S.contentWrapperSize[size],
       )}
       id={tooltipId}
       ref={contentRef}
       role='tooltip'
     >
-      <BubblePointIcon className={clsx(S.bubblePointIcon, S.bubblePointIconPosition[position])} />
+      <BubblePointIcon className={clsx(S.bubblePointIcon, S.bubblePointIconPosition[placement])} />
       <span className={S.content}>{children}</span>
     </div>
   );
