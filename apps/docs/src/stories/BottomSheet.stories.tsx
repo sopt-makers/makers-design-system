dc2efe333ae89fa64eec463bdef66467f784c938
최주용 <wuzoo@MacBook-Pro.local>
2026-01-10 15:53:29 +0900

    feat: storybook 개선

--- apps/docs/src/stories/BottomSheet.stories.tsx
+++ apps/docs/src/stories/BottomSheet.stories.tsx
@@ -4,26 +4,57 @@ import {
   BottomSheetContent,
   BottomSheetRoot,
   BottomSheetTrigger,
-  Button,
 } from '@sopt-makers/ui';
 import type { Meta, StoryObj } from '@storybook/react';
-import { useState } from 'react';
 
-const meta: Meta = {
+type SampleProps = {
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+  defaultOpen?: boolean;
+  title?: string;
+  backIcon?: boolean;
+};
+
+const meta: Meta<SampleProps> = {
   title: 'Components/BottomSheet',
   tags: ['autodocs'],
+  parameters: {
+    docs: {
+      codePanel: true,
+    },
+  },
+  args: {
+    defaultOpen: false,
+    title: 'Title',
+    backIcon: false,
+  },
+  argTypes: {
+    open: { type: 'boolean', control: { type: 'boolean' }, description: '`boolean`' },
+    onOpenChange: {
+      type: 'function',
+      control: { type: 'function' },
+      description: '`(open:boolean) => void`',
+    },
+    defaultOpen: {
+      type: 'boolean',
+      control: { type: 'boolean' },
+      description: '`boolean`',
+    },
+    title: { type: 'string', control: { type: 'string' }, description: '`string`' },
+    backIcon: { type: 'boolean', control: { type: 'boolean' }, description: '`boolean`' },
+  },
 };
 export default meta;
 
-export const BottomSheetUncontrolled: StoryObj = {
-  name: 'BottomSheet - Uncontrolled',
-  render: () => {
+export const BottomSheetDefault: StoryObj<SampleProps> = {
+  name: 'BottomSheet - Default',
+  render: (args) => {
     return (
-      <BottomSheetRoot>
+      <BottomSheetRoot open={args.open} onOpenChange={args.onOpenChange} defaultOpen={args.defaultOpen}>
         <BottomSheetTrigger>
-          <Button size='sm'>열기</Button>
+          <button>열기</button>
         </BottomSheetTrigger>
-        <BottomSheetContent title='Title'>
+        <BottomSheetContent title={args.title} backIcon={args.backIcon}>
           <BottomSheetBody>
             <p style={{ color: 'white' }}>
               커뮤니티 이용규칙은 누구나 기분 좋게 참여할 수 있는 커뮤니티를 만들기 위해 제정되었습니다. 서비스 내의
@@ -39,42 +70,14 @@ export const BottomSheetUncontrolled: StoryObj = {
   },
 };
 
-const ControlledSample = () => {
-  const [isOpen, setOpen] = useState(false);
-
-  return (
-    <BottomSheetRoot open={isOpen} onOpenChange={setOpen}>
-      <BottomSheetTrigger>
-        <Button size='sm'>{isOpen ? '닫기' : '열기'}</Button>
-      </BottomSheetTrigger>
-      <BottomSheetContent title='Title'>
-        <BottomSheetBody>
-          <p style={{ color: 'white' }}>
-            커뮤니티 이용규칙은 누구나 기분 좋게 참여할 수 있는 커뮤니티를 만들기 위해 제정되었습니다. 서비스 내의 모든
-            커뮤니티는 커뮤니티 이용규칙에 의해 운영되므로, 이용자는 커뮤니티 이용 전 반드시 모든 내용을 숙지하여야
-            합니다. 방송통신심의위원회의 정보통신에 관한 심의규정, 현행 법률, 서비스 이용약관 및 커뮤니티 이용규칙을
-            위반하거나, 사회 통념 및 관련 법령을 기준으로 타 이용자에게 악영향을 끼치는 경우, 게시물이 삭제되고 서비스
-            이용이 일정 기간 제한될 수 있습니다.
-          </p>
-        </BottomSheetBody>
-      </BottomSheetContent>
-    </BottomSheetRoot>
-  );
-};
-
-export const BottomSheetControlled: StoryObj = {
-  name: 'BottomSheet - Controlled',
-  render: ControlledSample,
-};
-
-export const BottomSheetBackIcon: StoryObj = {
+export const BottomSheetBackIcon: StoryObj<SampleProps> = {
   name: 'BottomSheet - Back Icon',
-  render: () => (
-    <BottomSheetRoot>
+  render: (args) => (
+    <BottomSheetRoot open={args.open} onOpenChange={args.onOpenChange} defaultOpen={args.defaultOpen}>
       <BottomSheetTrigger>
-        <Button size='sm'>열기</Button>
+        <button>열기</button>
       </BottomSheetTrigger>
-      <BottomSheetContent title='Title' backIcon>
+      <BottomSheetContent title={args.title} backIcon={args.backIcon}>
         <BottomSheetBody>
           <p style={{ color: 'white' }}>
             커뮤니티 이용규칙은 누구나 기분 좋게 참여할 수 있는 커뮤니티를 만들기 위해 제정되었습니다. 서비스 내의 모든
@@ -87,16 +90,19 @@ export const BottomSheetBackIcon: StoryObj = {
       </BottomSheetContent>
     </BottomSheetRoot>
   ),
+  args: {
+    backIcon: true,
+  },
 };
 
-export const BottomSheetWithActionButton: StoryObj = {
+export const BottomSheetWithActionButton: StoryObj<SampleProps> = {
   name: 'BottomSheet - Action Button',
-  render: () => (
-    <BottomSheetRoot>
+  render: (args) => (
+    <BottomSheetRoot open={args.open} onOpenChange={args.onOpenChange} defaultOpen={args.defaultOpen}>
       <BottomSheetTrigger>
-        <Button size='sm'>열기</Button>
+        <button>열기</button>
       </BottomSheetTrigger>
-      <BottomSheetContent title='Title'>
+      <BottomSheetContent title={args.title} backIcon={args.backIcon}>
         <BottomSheetBody>
           <p style={{ color: 'white' }}>
             커뮤니티 이용규칙은 누구나 기분 좋게 참여할 수 있는 커뮤니티를 만들기 위해 제정되었습니다. 서비스 내의 모든
@@ -112,14 +118,14 @@ export const BottomSheetWithActionButton: StoryObj = {
   ),
 };
 
-export const BottomSheetBodyScrolled: StoryObj = {
+export const BottomSheetBodyScrolled: StoryObj<SampleProps> = {
   name: 'BottomSheet - Body Scrolled',
-  render: () => (
-    <BottomSheetRoot>
+  render: (args) => (
+    <BottomSheetRoot open={args.open} onOpenChange={args.onOpenChange} defaultOpen={args.defaultOpen}>
       <BottomSheetTrigger>
-        <Button size='sm'>열기</Button>
+        <button>열기</button>
       </BottomSheetTrigger>
-      <BottomSheetContent title='Title'>
+      <BottomSheetContent title={args.title} backIcon={args.backIcon}>
         <BottomSheetBody>
           <p style={{ color: 'white' }}>
             커뮤니티 이용규칙은 누구나 기분 좋게 참여할 수 있는 커뮤니티를 만들기 위해 제정되었습니다. 서비스 내의 모든
