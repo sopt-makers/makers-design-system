d8bae08078fe2fac65485b968766fd934363dfce
rtttr1 <bobo3766@naver.com>
2026-01-11 16:44:04 +0900

    refactor: IconGenerator에 iconName prop으로 넘겨주는 방식으로 개선

--- apps/docs/src/stories/Icons.stories.tsx
+++ apps/docs/src/stories/Icons.stories.tsx
@@ -11,24 +11,42 @@ import {
   Logo,
 } from '@sopt-makers/icons';
 
-const convertIconNameToKebabCase = (iconName: string): string => {
-  const withoutIcon = iconName.replace(/^Icon/, '');
+// icon 카테고리 추가시 이곳에 추가
+const iconCategories = [
+  { name: 'Communication', icons: Communication },
+  { name: 'Editor', icons: Editor },
+  { name: 'Feedback', icons: Feedback },
+  { name: 'Files', icons: Files },
+  { name: 'General', icons: General },
+  { name: 'Interaction', icons: Interaction },
+  { name: 'Media', icons: Media },
+  { name: 'Time', icons: Time },
+  { name: 'Users', icons: Users },
+  { name: 'Logo', icons: Logo },
+];
 
-  return withoutIcon
+const convertIconNameToKebabCase = (iconName: string): string => {
+  const withoutIconPrefix = iconName.replace(/^Icon/, '');
+
+  return withoutIconPrefix
     .replace(/([A-Z])/g, '-$1')
     .toLowerCase()
     .replace(/^-/, '');
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const IconGenerator = ({ icon, style }: { icon: any; style: React.CSSProperties }) => {
-  const IconComponent = icon;
-  const iconName = convertIconNameToKebabCase(icon.__docgenInfo.displayName);
-
+const IconGenerator = ({
+  IconComponent,
+  style,
+  iconName,
+}: {
+  IconComponent: React.ComponentType<React.HTMLAttributes<SVGSVGElement>>;
+  style: React.CSSProperties;
+  iconName: string;
+}) => {
   return (
     <div>
       <IconComponent style={style} />
-      <p>{iconName}</p>
+      <p>{convertIconNameToKebabCase(iconName)}</p>
     </div>
   );
 };
@@ -40,27 +58,18 @@ export const Default = {
   render: (props: { color: string }) => {
     const style = { width: 24, height: 24, color: props.color };
 
-    // icon 카테고리 추가시 이곳에 추가
-    const iconCategories = [
-      { name: 'Communication', icons: Communication },
-      { name: 'Editor', icons: Editor },
-      { name: 'Feedback', icons: Feedback },
-      { name: 'Files', icons: Files },
-      { name: 'General', icons: General },
-      { name: 'Interaction', icons: Interaction },
-      { name: 'Media', icons: Media },
-      { name: 'Time', icons: Time },
-      { name: 'Users', icons: Users },
-      { name: 'Logo', icons: Logo },
-    ];
-
     return (
       <div className='icons-wrap'>
         {iconCategories.map((category) => (
           <div key={category.name} className='icons-group'>
             <h4>{category.name}</h4>
-            {Object.values(category.icons).map((icon, index) => (
-              <IconGenerator icon={icon} style={style} key={`${category.name}-${index}`} />
+            {Object.entries(category.icons).map(([iconName, icon]) => (
+              <IconGenerator
+                IconComponent={icon}
+                style={style}
+                key={`${category.name}-${iconName}`}
+                iconName={iconName}
+              />
             ))}
           </div>
         ))}
