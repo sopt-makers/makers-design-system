e2ea2349f4d2faed26619e1f91be1617e8ece89f
jeonghoon11 <jih361@naver.com>
2026-04-27 19:00:41 +0900

    refactor: isExpanded를 isExtended로 네이밍 변경 및 padding과 borderRadius 로직 분리

--- packages/ui/Button/utils.ts
+++ packages/ui/Button/utils.ts
@@ -1,12 +1,24 @@
 import { sprinkles } from './style.css';
-import type { ButtonIntent, ButtonSizeTheme, ButtonShape, ButtonColorTheme, ButtonVariant } from './types';
+import type { ButtonIntent, ButtonSizeTheme, ButtonShape, ButtonVariant } from './types';
+
+interface BorderRadiusParams {
+  radiusTheme: ButtonShape;
+  sizeTheme: ButtonSizeTheme;
+  variant: ButtonVariant;
+}
+
+interface PaddingParams {
+  isExtended: boolean;
+  sizeTheme: ButtonSizeTheme;
+  variant: ButtonVariant;
+}
 
 function createButtonVariant(
   buttonIntent: ButtonIntent,
   radiusTheme: ButtonShape,
   sizeTheme: ButtonSizeTheme,
   variant: ButtonVariant,
-  isExpanded: boolean,
+  isExtended: boolean,
 ) {
   return sprinkles({
     backgroundColor: {
@@ -27,17 +39,26 @@ function createButtonVariant(
       active: `${variant}-${buttonIntent}-press`,
       disabled: `${variant}-disabled`,
     },
-    borderRadius: variant === 'text' || variant === 'floating' ? variant : radiusTheme === 'pill' ? 'max' : sizeTheme,
-    padding:
-      variant === 'floating'
-        ? isExpanded
-          ? 'floating-extended'
-          : 'floating-default'
-        : variant === 'text'
-          ? 'text'
-          : sizeTheme,
+    borderRadius: getBorderRadius({ radiusTheme, sizeTheme, variant }),
+    padding: getPadding({ isExtended, sizeTheme, variant }),
     fontSize: sizeTheme,
   });
+}
+
+function getBorderRadius({ radiusTheme, sizeTheme, variant }: BorderRadiusParams) {
+  if (variant === 'text' || variant === 'floating') return variant;
+  if (radiusTheme === 'pill') return 'max';
+  return sizeTheme;
+}
+
+function getPadding({ isExtended, sizeTheme, variant }: PaddingParams) {
+  if (variant === 'floating') {
+    return isExtended ? 'floating-extended' : 'floating-default';
+  }
+
+  if (variant === 'text') return 'text';
+
+  return sizeTheme;
 }
 
 export default createButtonVariant;
